1396a7be263a510d6645776f5b99794bd2d05f2e
macreat <mateomacreat@gmail.com>
2025-11-07 19:18:47 -0500

    second assignment : prooving three matches cases

--- devEnv/work/2cndAssignment/tb/tb_top.v
+++ devEnv/work/2cndAssignment/tb/tb_top.v
@@ -22,14 +22,35 @@ module tb_top;
   always #312.5 clk = ~clk; // 1.6 MHz
 
   initial begin
-    $dumpfile("results/tb_top.vcd");
+    $dumpfile("results/tb_top.vcd");  
     $dumpvars(0, tb_top);
     rst_n = 0; #2000; rst_n = 1;
 
-    send_uart_byte(8'b01100110);
-    #200000;
+       //----------------------------------------------------------
+    // CASO 1: Sin match
+    // Byte 0xFF (11111111) no contiene "0110" → match = 0
+    //----------------------------------------------------------
+    send_uart_byte(8'b11111111);
+    #200000; // esperar 200 µs
 
-    #500000;
+    //----------------------------------------------------------
+    // CASO 2: Matches seguidos
+    // 0x66 (01100110) contiene el patrón dos veces seguidas
+    // Se esperan dos pulsos de match
+    //----------------------------------------------------------
+    send_uart_byte(8'b01100110);
+    #200000; // separar un poco
+
+
+    //----------------------------------------------------------
+    // CASO 3: Match al inicio y otro al final
+    // 0x66 produce match temprano
+    // 0x60 (01100000) termina en "0110" también
+    //----------------------------------------------------------
+    send_uart_byte(8'b00000110);  // match al inicio
+    send_uart_byte(8'b01100000);  // match al final
+    #500000; // dejar tiempo al final
+
     $finish;
   end
 
